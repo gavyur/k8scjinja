432d56bccfcb30a3a23c53c1b6cb5efd548604c9
Yury Gavrilov <gavyur@users.noreply.github.com>
2023-12-26 21:31:59 +0000

    Add environment file settings

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # k8scjinja
 Docker app for rendering jinja templates from any files using yaml-environment
 # Installation
-TBD
+`docker pull gavyur/k8scjinja:latest`
 # Building on your own
 `docker build --rm -t k8scjinja:latest -f Dockerfile .`
 # Running
-`docker run -i -v $(pwd):/pwd k8scjinja:latest -t example/config.yaml.template < example/env-example.yaml`
+`docker run -i -v $(pwd):/pwd gavyur/k8scjinja:latest -t example/config.yaml.template < example/env-example.yaml`
--- k8scjinja.py
+++ k8scjinja.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='ProgramName', description='What the program does', epilog='Text at the bottom of help')
     parser.add_argument('-t', '--template_filename', required=True)
     parser.add_argument('-d', '--destination_filename', required=False)
+    parser.add_argument('-e', '--environment_filename', required=False)
+    parser.add_argument('-i', '--input', action='store_true')
     args = parser.parse_args()
 
     if not os.path.isfile(args.template_filename):
@@ -15,12 +17,21 @@ if __name__ == '__main__':
         sys.exit(1)
     if args.destination_filename and not os.path.isfile(args.destination_filename):
         print(f'File <{args.destination_filename}> not found. Please, check that you provided the path relatively to $PWD variable.')
-        sys.exit(1)
+        sys.exit(2)
 
-    print('Waiting for environment in stdin...', end=' ')
-    str_env = sys.stdin.read()
+    if args.input:
+        print('Waiting for environment in stdin...', end=' ')
+        str_env = sys.stdin.read()
+        print('received')
+    else:
+        environment_filename = args.environment_filename
+        if environment_filename is None:
+            environment_filename = os.path.expanduser('~/.k8scjinja.env.yaml')
+        if not os.path.isfile(environment_filename):
+            print(f'Environment file <{environment_filename}> not found. Please, specify correct environment file with -e option')
+            sys.exit(3)
+
     environment = yaml.load(str_env, yaml.FullLoader)
-    print('received')
 
     print(f'Rendering <{args.template_filename}>...', end=' ')
     with open(args.template_filename, 'r') as fp:
